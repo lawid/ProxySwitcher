cca0d9329b904ed288ee09698b7605752c24b23c
Lawrence Emory Dean <lawidean@gmail.com>
2015-09-21 17:34:10 +0200

    Work on listener for connectivity change

--- TriggerPlayground/Program.cs
+++ TriggerPlayground/Program.cs
@@ -5,42 +5,63 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.NetworkInformation;
+using SimpleWifi;
+
 namespace ConsoleApplication1
 {
     class Program
     {
+        private static Wifi wifi;
+
         public static void Main()
         {
-            NetworkChange.NetworkAddressChanged += new
-            NetworkAddressChangedEventHandler(AddressChangedCallback);
-            NetworkChange.NetworkAvailabilityChanged += new NetworkAvailabilityChangedEventHandler(AvailabilityChanged);
+            wifi = new Wifi();
+            // listen to wifi changes
+            wifi.ConnectionStatusChanged += wifi_connectionStatusChanged;
+
+            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(AddressChangedCallback);
             Console.WriteLine("Listening...");
             Console.ReadLine();
         }
 
-        private static void AvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        private static void wifi_connectionStatusChanged(object sender, WifiStatusEventArgs e)
         {
-            Console.WriteLine("availability changed");
-            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
-            foreach (NetworkInterface n in adapters)
-            {
-
-                Console.WriteLine(" {0} is {1}", n.Name, n.OperationalStatus);
-            }
+            Console.WriteLine("wifi changed");
+            if(e.NewStatus==WifiStatus.Connected)
+            HandleListenEvent();
         }
-
 
         static void AddressChangedCallback(object sender, EventArgs e)
         {
+            Console.WriteLine("address changed");
 
-
-
-            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
-            foreach (NetworkInterface n in adapters)
-            {
-
-                Console.WriteLine(" {0} is {1}", n.Name, n.OperationalStatus);
-            }
         }
+
+        private static bool HandleListenEvent()
+        {
+            if(wifi.ConnectionStatus == WifiStatus.Connected)
+            {
+                List<AccessPoint> aps = wifi.GetAccessPoints();
+                foreach(AccessPoint ap in aps)
+                {
+                    if(ap.IsConnected)
+                    {
+                        Console.WriteLine("i'm connected to " + ap.Name);
+                        if(ap.Name=="WLAN-503v")
+                        {
+                            Console.WriteLine("That's mine enable!!!");
+                            return true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("not for me, disable");
+            }
+            return false;
+        }
+        
+        
     }
 }
